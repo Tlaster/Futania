17d05bbe41049733c50588e88807e7f68ea78b35
tlaster <tlaster@outlook.com>
2019-10-08 17:49:18 +0800

    update

--- Android/app/src/main/java/moe/tlaster/futania/common/bindingAdapter/Generic.kt
+++ Android/app/src/main/java/moe/tlaster/futania/common/bindingAdapter/Generic.kt
@@ -1,2 +1,17 @@
 package moe.tlaster.futania.common.bindingAdapter
 
+import android.view.View
+import androidx.core.view.updatePaddingRelative
+import androidx.databinding.BindingAdapter
+import moe.tlaster.futania.common.dp
+
+
+@BindingAdapter("paddingHorizontal")
+fun paddingHorizontal(view: View, int: Int) {
+    view.updatePaddingRelative(start = int.dp.toInt(), end = int.dp.toInt())
+}
+
+@BindingAdapter("paddingVertical")
+fun paddingVertical(view: View, int: Int) {
+    view.updatePaddingRelative(top = int.dp.toInt(), bottom = int.dp.toInt())
+}
--- Android/app/src/main/java/moe/tlaster/futania/common/bindingAdapter/WebView.kt
+++ Android/app/src/main/java/moe/tlaster/futania/common/bindingAdapter/WebView.kt
@@ -1,2 +1,38 @@
 package moe.tlaster.futania.common.bindingAdapter
 
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("source")
+fun source(webView: WebView, url: String) {
+    webView.settings.javaScriptEnabled = true
+    webView.loadUrl(url)
+}
+
+private val BINDINGWEBVIEWCLIENT_ID = -811
+
+@BindingAdapter("onPageFinished")
+fun onPageFinished(webView: WebView, callback: (url: String?) -> Unit) {
+    if (webView.getTag(BINDINGWEBVIEWCLIENT_ID) !is BindingWebViewClient) {
+        BindingWebViewClient().let {
+            webView.webViewClient = it
+            webView.setTag(BINDINGWEBVIEWCLIENT_ID, it)
+        }
+    }
+    webView.getTag(BINDINGWEBVIEWCLIENT_ID)?.let {
+        it as? BindingWebViewClient
+    }?.let {
+        it.pageFinished = callback
+    }
+}
+
+
+class BindingWebViewClient : WebViewClient() {
+
+    var pageFinished: ((url: String?) -> Unit)? = null
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        pageFinished?.invoke(url)
+    }
+}
